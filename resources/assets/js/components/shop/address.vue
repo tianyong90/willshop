98feb973065d3535283970c412438b05db0e6839
tianyong90 <412039588@qq.com>
2016-08-19 11:06:22 +0800

    禁用页面缩放

--- resources/assets/js/components/shop/address.vue
+++ resources/assets/js/components/shop/address.vue
@@ -1,6 +1,6 @@
 <template>
     
-
+    addres
 </template>
 
 <script>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="keywords" content="">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximun-scale=1.0,user-scalable=0">
+    <meta name="format-detection" content="telephone=no" />
     <title>willshop</title>
     <!-- Set render engine for 360 browser -->
     <meta name="renderer" content="webkit">
