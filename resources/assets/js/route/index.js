2d6081926a57b60d2bc14e30fceab97e7f787ed3
tianyong90 <412039588@qq.com>
2017-01-04 16:08:30 +0800

    结构整理

---
+++ resources/assets/js/route/index.js
@@ -0,0 +1,80 @@
+router.map({
+    '/': {
+        component: require('./components/shop/home.vue')
+    },
+    '/cart': {
+        component: require('./components/shop/cart.vue'),
+        auth: true
+    },
+    '/category': {
+        component: require('./components/shop/category.vue')
+    },
+    '/order-list': {
+        component: require('./components/shop/order-list.vue'),
+        auth: true
+    },
+    '/order/:id': {
+        component: require('./components/shop/order.vue'),
+        auth: true
+    },
+    '/favourite': {
+        component: require('./components/shop/favourite.vue'),
+        auth: true
+    },
+    '/checkout': {
+        component: require('./components/shop/checkout.vue'),
+        auth: true
+    },
+    '/user': {
+        component: require('./components/shop/user.vue'),
+        auth: true
+    },
+    '/profile': {
+        component: require('./components/shop/profile.vue'),
+        auth: true
+    },
+    '/avatar': {
+        component: require('./components/shop/avatar.vue'),
+        auth: true,
+        hideMainmenu: true
+    },
+    '/address': {
+        component: require('./components/shop/address.vue'),
+        auth: true
+    },
+    '/address/add': {
+        component: require('./components/shop/address-edit.vue'),
+        auth: true,
+        hideMainmenu: true
+    },
+    '/address/:id': {
+        component: require('./components/shop/address-edit.vue'),
+        auth: true,
+        hideMainmenu: true
+    },
+    '/about-us': {
+        component: require('./components/shop/about-us.vue')
+    },
+    '/help': {
+        component: require('./components/shop/help.vue')
+    },
+    '/help/:id': {
+        component: require('./components/shop/help-detail.vue')
+    },
+    '/login': {
+        component: require('./components/shop/login.vue'),
+        hideMainmenu: true
+    },
+    '/register': {
+        component: require('./components/shop/register.vue'),
+        hideMainmenu: true
+    },
+    '/product/:id': {
+        component: require('./components/shop/product.vue'),
+        hideMainmenu: true
+    },
+    '/password': {
+        component: require('./components/shop/password.vue'),
+        auth: true
+    }
+});
--- resources/assets/js/shop.js
+++ resources/assets/js/shop.js
@@ -1,15 +1,6 @@
 window._ = require('lodash');
 
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-window.$ = window.jQuery = require('jquery');
-require('bootstrap-sass');
-
-/**
  * Vue is a modern JavaScript library for building interactive web interfaces
  * using reactive data binding and reusable components. Vue's API is clean
  * and simple, leaving you to focus on building your next great project.
@@ -29,25 +20,6 @@ Vue.http.interceptors.push((request, next) => {
 
     next();
 });
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from "laravel-echo"
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the body of the page. From here, you may begin adding components to
- * the application, or feel free to tweak this setup for your needs.
- */
 
 Vue.component('mainmenu', require('./components/shop/mainmenu.vue'));
 
@@ -145,87 +117,6 @@ Vue.http.interceptors.push((request, next) => {
             router.go('/login');
         }
     });
-});
-
-router.map({
-    '/': {
-        component: require('./components/shop/home.vue')
-    },
-    '/cart': {
-        component: require('./components/shop/cart.vue'),
-        auth: true
-    },
-    '/category': {
-        component: require('./components/shop/category.vue')
-    },
-    '/order-list': {
-        component: require('./components/shop/order-list.vue'),
-        auth: true
-    },
-    '/order/:id': {
-        component: require('./components/shop/order.vue'),
-        auth: true
-    },
-    '/favourite': {
-        component: require('./components/shop/favourite.vue'),
-        auth: true
-    },
-    '/checkout': {
-        component: require('./components/shop/checkout.vue'),
-        auth: true
-    },
-    '/user': {
-        component: require('./components/shop/user.vue'),
-        auth: true
-    },
-    '/profile': {
-        component: require('./components/shop/profile.vue'),
-        auth: true
-    },
-    '/avatar': {
-        component: require('./components/shop/avatar.vue'),
-        auth: true,
-        hideMainmenu: true
-    },
-    '/address': {
-        component: require('./components/shop/address.vue'),
-        auth: true
-    },
-    '/address/add': {
-        component: require('./components/shop/address-edit.vue'),
-        auth: true,
-        hideMainmenu: true
-    },
-    '/address/:id': {
-        component: require('./components/shop/address-edit.vue'),
-        auth: true,
-        hideMainmenu: true
-    },
-    '/about-us': {
-        component: require('./components/shop/about-us.vue')
-    },
-    '/help': {
-        component: require('./components/shop/help.vue')
-    },
-    '/help/:id': {
-        component: require('./components/shop/help-detail.vue')
-    },
-    '/login': {
-        component: require('./components/shop/login.vue'),
-        hideMainmenu: true
-    },
-    '/register': {
-        component: require('./components/shop/register.vue'),
-        hideMainmenu: true
-    },
-    '/product/:id': {
-        component: require('./components/shop/product.vue'),
-        hideMainmenu: true
-    },
-    '/password': {
-        component: require('./components/shop/password.vue'),
-        auth: true
-    }
 });
 
 router.start(App, 'body');
