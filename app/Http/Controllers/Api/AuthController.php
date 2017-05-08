2d89ba697d2b3fcfcb2b4c465d87ca0b85a5c1b2
hexu <hexu.garfield@gmail.com>
2017-05-08 21:57:26 +0800

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -45,7 +45,8 @@ class AutHController extends BaseController
     public function register(Request $request)
     {
         $data = $request->all();
-
+        $data['password'] = bcrypt($request->password);
+        
         $rules = [
             'name' => 'required|min:3|max:20',
             'mobile' => 'required|min:1|max:12',
@@ -103,7 +104,7 @@ class AutHController extends BaseController
     public function updatePassword(Request $request)
     {
         $data = $request->all();
-        $data['password'] = $request->password;
+
 
         try {
             if (!$user = JWTAuth::parseToken()->authenticate()) {
