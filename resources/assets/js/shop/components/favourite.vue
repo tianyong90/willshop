ac8cf8ea14987cfd56df9b1703216aa7ead43a76
tianyong90 <412039588@qq.com>
2017-05-26 00:00:04 +0800

    收藏页调整

--- resources/assets/js/shop/components/favourite.vue
+++ resources/assets/js/shop/components/favourite.vue
@@ -1,13 +1,17 @@
 <template>
-  <ul id="favourite-list">
-    <router-link tag="li" :to="'product/' + favourite.product.id" class="list-item" v-for="favourite in favourites" :key="favourite">
-      <img :src="favourite.product.thumbnail" alt="" class="thumbnail" :to="'/product/' + favourite.product.id">
-      <div class="right-part">
-        <div class="name" :to="'/product/' + favourite.product.id">{{ favourite.product.name }}</div>
-        <span class="price">{{ favourite.product.price }}</span>
-      </div>
-    </router-link>
-  </ul>
+  <div class="weui-panel weui-panel_access">
+    <div class="weui-panel__bd">
+      <router-link :to="'/product/' + favourite.product.id" tag="div" class="weui-media-box weui-media-box_appmsg" v-for="favourite in favourites">
+        <div class="weui-media-box__hd">
+          <img class="weui-media-box__thumb" :src="favourite.product.thumbnail">
+        </div>
+        <div class="weui-media-box__bd">
+          <h4 class="weui-media-box__title" v-text="favourite.product.name"></h4>
+          <p class="weui-media-box__desc price" v-text="favourite.product.price"></p>
+        </div>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,93 +39,7 @@
 </script>
 
 <style scoped lang="scss">
-  #favourite-list {
-    display: block;
-    margin: 0;
-    padding: 0;
-    background-color: #fff;
-
-    .list-item {
-      display: block;
-      overflow: hidden;
-      padding: 10px;
-      border-bottom: 1px solid #eee;
-
-      .checker {
-        display: inline-block;
-        float: left;
-        margin: 30px 15px 0 10px;
-      }
-
-      .thumbnail {
-        display: block;
-        float: left;
-        width: 80px;
-        height: 80px;
-        border: 1px solid #eee;
-      }
-
-      .right-part {
-        position: relative;
-        display: block;
-        overflow: hidden;
-        height: 80px;
-        float: left;
-        padding: 0 10px;
-
-        .name {
-          font-size: 15px;
-        }
-
-        .price {
-          position: absolute;
-          bottom: 0;
-          color: #f00;
-        }
-      }
-    }
-  }
-
-  footer {
-    display: block;
-    position: fixed;
-    bottom: 55px;
-    width: 100%;
-    background-color: #fff;
-    height: 50px;
-
-    #check-all {
-      float: left;
-      margin: 11px 10px;
-      font-size: 13px;
-    }
-
-    .summary {
-      float: left;
-      padding-left: 10px;
-    }
-
-    .total-price {
-      color: #f00;
-      font-size: 15px;
-    }
-
-    .product-count {
-      font-size: 13px;
-    }
-
-    #btn-checkout {
-      display: block;
-      float: right;
-      color: #fff;
-      line-height: 50px;
-      padding: 0 20px;
-      background-color: #f00;
-      border: none;
-
-      &.disabled {
-        background-color: #ccc;
-      }
-    }
+  .price {
+    color: #f44336;
   }
 </style>
