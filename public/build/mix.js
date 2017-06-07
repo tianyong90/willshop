407d599d39a28fba128783a2c50dcf1344fc2a15
tianyong90 <412039588@qq.com>
2017-06-08 01:03:19 +0800

    商品详情页调整

---
+++ public/build/mix.js
@@ -0,0 +1,18 @@
+webpackJsonp([2],{
+
+/***/ 127:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(54);
+
+
+/***/ }),
+
+/***/ 54:
+/***/ (function(module, exports) {
+
+eval("//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNTQuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=");
+
+/***/ })
+
+},[127]);
