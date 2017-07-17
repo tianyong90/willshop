1ebb8db4387a2e9188a74935eb018059920a6d5d
tianyong90 <412039588@qq.com>
2017-07-18 02:01:11 +0800

    eslint 调整，不使用分号

--- public/build/mix.js
+++ public/build/mix.js
@@ -1,18 +1,18 @@
 webpackJsonp([20],{
 
-/***/ 55:
+/***/ 56:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(56);
+module.exports = __webpack_require__(57);
 
 
 /***/ }),
 
-/***/ 56:
+/***/ 57:
 /***/ (function(module, exports) {
 
-eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNTYuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///56\n");
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNTcuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///57\n");
 
 /***/ })
 
-},[55]);
+},[56]);
--- resources/assets/js/shop/store/index.js
+++ resources/assets/js/shop/store/index.js
@@ -1,29 +1,29 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 const state = {
   isLoading: false,
   direction: 'forward',
   isMainMenuVisible: true,
   isLogin: false
-};
+}
 
 export default new Vuex.Store({
   state,
   mutations: {
     UPDATE_LOADING (state, value) {
-      state.isLoading = value;
+      state.isLoading = value
     },
     UPDATE_DIRECTION (state, value) {
-      state.direction = value;
+      state.direction = value
     },
     UPDATE_MAINMENU_VISIBLE (state, value) {
-      state.isMainMenuVisible = value;
+      state.isMainMenuVisible = value
     },
     UPDATE_IS_LOGIN (state, value) {
-      state.isLogin = value;
+      state.isLogin = value
     }
   }
-});
+})
