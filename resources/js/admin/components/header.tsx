b8e75f2e4ae01a553a6b031e8ff2d51b888f4af3
tianyong90 <412039588@qq.com>
2019-10-16 16:01:52 +0800

    webpack config

--- resources/js/admin/components/header.tsx
+++ resources/js/admin/components/header.tsx
@@ -1,36 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component, FC } from 'react'
 import { Layout, Menu, Avatar } from 'antd'
 
 const { Header } = Layout
 
-export default class Sidebar extends Component {
-  constructor (props) {
-    super(props)
-  }
+const MyHeader: React.FC = () => {
+  return (
+    <Header className="flex items-center header">
+      <div className="inline-flex text-white text-lg font-medium">WILLSHOP</div>
+      <Menu
+        className="ml-auto mr-1"
+        theme="dark"
+        mode="horizontal"
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="index">首页</Menu.Item>
+      </Menu>
 
-  componentDidMount (): void {
-  }
-
-  public render () {
-    return (
-      <Header className="flex items-center header">
-        <div className="inline-flex text-white text-lg font-medium">WILLSHOP</div>
-        <Menu
-          className="ml-auto mr-1"
-          theme="dark"
-          mode="horizontal"
-          style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="index">首页</Menu.Item>
-          <Menu.Item key="index2">首页</Menu.Item>
-          <Menu.Item key="index3">首页</Menu.Item>
-        </Menu>
-
-        <Avatar
-          className="ml-1"
-          icon="user"
-        />
-      </Header>
-    )
-  }
+      <Avatar
+        className="ml-1"
+        style={{ verticalAlign: 'middle', backgroundColor: 'red' }}
+      >bontian</Avatar>
+    </Header>
+  )
 }
+
+export default MyHeader
