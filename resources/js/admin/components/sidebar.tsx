6190b9ca30908775b17d98c2e92c1e1fe7368ab1
tianyong90 <412039588@qq.com>
2019-10-21 04:53:40 +0800

    sidebar menu

--- resources/js/admin/components/sidebar.tsx
+++ resources/js/admin/components/sidebar.tsx
@@ -4,88 +4,115 @@ import { Icon, Layout, Menu } from 'antd'
 const { Sider } = Layout
 const { SubMenu } = Menu
 
-export default class Sidebar extends Component {
-  constructor (props) {
-    super(props)
-  }
+const menus = [
+  {
+    name: '控制面板',
+    icon: 'home',
+    key: 'home',
+  },
+  {
+    name: '订单管理',
+    icon: 'order',
+    key: 'order',
+  },
+  {
+    name: '用户管理',
+    icon: 'user',
+    key: 'user',
+  },
+  {
+    name: '商品管理',
+    icon: 'goods',
+    key: 'goods',
+  },
+]
 
-  componentDidMount (): void {
-  }
-
-  public render () {
-    return (
-      <Sider
+const Sidebar = () => {
+  return (
+    <Sider
+      theme="dark"
+      collapsible
+      width={200}
+      style={{ background: '#f00' }}
+    >
+      <Menu
         theme="dark"
-        collapsible
-        width={200}
-        style={{ background: '#f00' }}
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        style={{ height: '100%', borderRight: 0 }}
       >
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
-        >
-          <Menu.Item key="home">
-            <span>
-                <Icon type="home" />
-                控制面板
+        {
+          menus.map(menu => (
+            <Menu.Item key={menu.key}>
+              <span>
+                <Icon type={menu.icon}/>
+                {menu.name}
               </span>
-          </Menu.Item>
+            </Menu.Item>
+          ))
+        }
 
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <Icon type="laptop" />
-                商品管理
-              </span>
-            }
-          >
-            <Menu.Item key="5">option5</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <Icon type="user" />
-                订单管理
-              </span>
-            }
-          >
-            <Menu.Item key="5">订单列表</Menu.Item>
-            <Menu.Item key="6">abc</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <Icon type="laptop" />
-                用户管理
-              </span>
-            }
-          >
-            <Menu.Item key="5">用户列表</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <Icon type="laptop" />
-                评论管理
-              </span>
-            }
-          >
-            <Menu.Item key="5">评论列表</Menu.Item>
-            <Menu.Item key="6">option6</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
-    )
-  }
+        <Menu.Item key="home">
+          <span>
+              <Icon type="home" />
+              控制面板
+            </span>
+        </Menu.Item>
+
+        <SubMenu
+          key="sub2"
+          title={
+            <span>
+              <Icon type="laptop" />
+              商品管理
+            </span>
+          }
+        >
+          <Menu.Item key="5">option5</Menu.Item>
+          <Menu.Item key="6">option6</Menu.Item>
+          <Menu.Item key="7">option7</Menu.Item>
+          <Menu.Item key="8">option8</Menu.Item>
+        </SubMenu>
+        <SubMenu
+          key="sub1"
+          title={
+            <span>
+              <Icon type="user" />
+              订单管理
+            </span>
+          }
+        >
+          <Menu.Item key="5">订单列表</Menu.Item>
+          <Menu.Item key="6">abc</Menu.Item>
+        </SubMenu>
+        <SubMenu
+          key="sub2"
+          title={
+            <span>
+              <Icon type="laptop" />
+              用户管理
+            </span>
+          }
+        >
+          <Menu.Item key="5">用户列表</Menu.Item>
+          <Menu.Item key="6">option6</Menu.Item>
+        </SubMenu>
+        <SubMenu
+          key="sub2"
+          title={
+            <span>
+              <Icon type="laptop" />
+              评论管理
+            </span>
+          }
+        >
+          <Menu.Item key="5">评论列表</Menu.Item>
+          <Menu.Item key="6">option6</Menu.Item>
+        </SubMenu>
+      </Menu>
+    </Sider>
+  )
 }
+
+export default Sidebar
